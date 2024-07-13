e7b59438485653c37e596f942c1194b62926aed5
Echelebo <echelebochibuike@gmail.com>
2024-07-13 08:18:42 +0100

    First Commit

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -1,5 +1,34 @@
 @extends('layouts.user')
+<style>
+    ul{
+        list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+    }
 
+  li {
+
+    float: right;
+  }
+
+  li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  background-color: #033766;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+  background-color: #111;
+}
+
+
+
+</style>
 @section('contents')
     <div class="container-fluid">
 
