4adbd8471a13304cf97ee11848c2ea2c39c1be2b
Echelebo <echelebochibuike@gmail.com>
2024-07-13 11:53:06 +0100

    First Commit

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -222,7 +222,7 @@ li a:hover {
                                 <p><font size="5">Balance {{ user()->balance }} TBC</font></p>
                                 <br />
                                 <p>qrcode here</p><br />
-                                <p align="center" style="background-color:#cac8c8; color: #1d1a1ad3; width:50%;"><font size="5">Wallet Address</font></p>
+                                <p style="background-color:#cac8c8; color: #1d1a1ad3; padding: 8px;"><font size="5">Wallet Address</font></p>
                                 </div>
 
                             </div>
