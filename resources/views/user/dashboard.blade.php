f9cc1517c09ab24be493e834d5f50b8a6a8d8a58
Echelebo <echelebochibuike@gmail.com>
2024-07-14 20:24:08 +0100

    First Commit

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -1,30 +1,43 @@
 @extends('layouts.user')
 <style>
-    ul{
-        float: right;
-    }
 
-  li {
-
-
-  }
-
-  li a {
-
+    .sidenav {
+  height: 100%;
+  width: 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  right: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  transition: 0.5s;
+  padding-top: 60px;
 }
 
-/* Change the link color to #111 (black) on hover */
-li a:hover {
-
+.sidenav a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
 }
 
-.menu-bodyx{
-    background: #fff;
-    Color: #111010;
-    width: 300px;
-    height: auto;
-    border-radius: 3%;
-    z-index: 291823;
+.sidenav a:hover {
+  color: #f1f1f1;
+}
+
+.sidenav .closebtn {
+  position: absolute;
+  top: 0;
+  right: 25px;
+  font-size: 36px;
+  margin-left: 50px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
 }
 
 </style>
@@ -188,28 +201,18 @@ li a:hover {
                                 </div>
                             <div class="col-md-6 py-4">
 
-                                <div class="menu-bodyx">
-                                    <ul class="text-center">
-                                        <li><a href=""><i class="fas fa-download" aria-hidden="true"></i>
-                                            <span>HOME</span></a></li>
-                                        <li><a href=""><i class="fas fa-upload" aria-hidden="true"></i>
-                                            <span>UPDATES</span></a></li>
-                                        <li><a href=""><i class="fas fa-bullhorn" aria-hidden="true"></i>
-                                            <span>REFERRALS</span></a></li>
-                                        <li><a href=""><i class="fa fa-search" aria-hidden="true"></i>
-                                            <span>EXPLORER</span></a></li>
-                                            <li><a href=""><i class="fa fa-search" aria-hidden="true"></i>
-                                                <span>SUBMIT BALANCE RECOVERY REQUEST</span></a></li>
-                                        <li><a href=""><i class="fas fa-sign-in-alt" aria-hidden="true"></i>
-                                            <span>LOGOUT</span></a></li>
-
-
-                                    </ul>
-                                </div>
-
-
-
+                                <div id="mySidenav" class="sidenav">
+                                    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;CLOSE</a>
+                                    <a href="#">HOME</a>
+                                    <a href="#">UPDATES</a>
+                                    <a href="#">REFERRALS</a>
+                                    <a href="#">EXPLORER</a>
+                                    <a href="#">SUBMIT BALANCE RECOVERY REQUEST</a>
+                                    <a href="#">LOGOUT</a>
+                                  </div>
                         </div>
+
+                        <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
 
 
 
@@ -235,6 +238,18 @@ li a:hover {
 @section('scripts')
     <script src="https://code.highcharts.com/highcharts.js"></script>
     <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+
+
+        <script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+</script>
+
 
     <script>
         var profits = {!! json_encode($profits) !!};
